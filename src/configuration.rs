9cbb1a10c39173bc3e695cb32b0a2f1f27cb9f47
Rodrigo Dominguez <rorra@rorra.com.ar>
2023-02-25 00:16:02 -0300

    Chapter 7 done

--- src/configuration.rs
+++ src/configuration.rs
@@ -18,7 +18,8 @@ pub struct Settings {
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
-    pub host: String
+    pub host: String,
+    pub base_url: String
 }
 
 #[derive(Clone)]
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -3,6 +3,8 @@ use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::{PgPool, Executor, Connection, PgConnection};
 use once_cell::sync::Lazy;
+use wiremock::MockServer;
+use uuid::Uuid;
 
 // `tracing` stack initialized only once per all the tests
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -17,9 +19,16 @@ static TRACING: Lazy<()> = Lazy::new(|| {
     };
 });
 
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
+}
+
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -32,14 +41,42 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub fn get_confirmation_links(
+        &self,
+        email_request: &wiremock::Request
+    ) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+
+            // Let's make sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+        let html = get_link(&body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(&body["TextBody"].as_str().unwrap());
+        ConfirmationLinks { html, plain_text }
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
-        c.database.database_name = "newsletter_test".to_string();
+        c.database.database_name = Uuid::new_v4().to_string();
+        c.email_client.base_url = email_server.uri();
         c.application.port = 0;
         c
     };
@@ -49,12 +86,15 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application.");
+    let application_port = application.port();
     let address = format!("http://127.0.0.1:{}", application.port());
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
         address,
+        port: application_port,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
@@ -66,7 +106,7 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
-        .unwrap_err();
+        .expect("Failed to create database.");
 
     // Migrate the db
     let connection_pool = PgPool::connect_with(config.with_db())
@@ -75,12 +115,7 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
-        .expect("Failed to migrate the database.");
-
-    sqlx::query!("TRUNCATE TABLE subscriptions")
-        .execute(&connection_pool)
-        .await
-        .expect("Failed to truncate subscriptions table");
+        .expect("Failed to migrate the database");
 
     connection_pool
 }
